f0df418200e33f20ea4f7246cd0b1593d113018f
Daniel <eoyeniran@tradedept.co>
2020-02-13 10:35:25 +0100

    have been able to set up the view and design the options button and questions div

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { arr } from "./questions";
+import "./app.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    disableButton: false,
+    pickedIndex: null,
+    questionsTotal: 0,
+    currentNumber: 0
+  };
+  componentDidMount = () => {
+    this.setState({ questionsTotal: arr.length });
+  };
+
+  selectAnswer = e => {
+    const picked = Number(e.target.value);
+    this.setState({ disableButton: true, pickedIndex: picked });
+    console.log(picked, e.target.id);
+  };
+
+  handlePrevious = () => {
+    const { currentNumber } = this.state;
+    if (currentNumber > 0) {
+      this.setState({ currentNumber: currentNumber - 1, disableButton: false });
+    }
+  };
+
+  handleNext = () => {
+    const { currentNumber } = this.state;
+    if (currentNumber < arr.length) {
+      this.setState({ currentNumber: currentNumber + 1, disableButton: false });
+    }
+  };
+
+  render() {
+    const { disableButton, pickedIndex, currentNumber } = this.state;
+    console.log(currentNumber);
+    return (
+      <div className="questionnaire">
+        <div>
+          <p className="question">{arr[currentNumber].question}</p> 
+          <div>
+            {arr[currentNumber].options.map((option, index) => {
+              return (
+                <button
+                  key={index}
+                  className={
+                    disableButton ? "option-button disabled" : "option-button"
+                  }
+                  onClick={this.selectAnswer}
+                  value={index}
+                  disabled={disableButton}
+                >
+                  {option}
+                </button>
+              );
+            })}
+          </div>
+        </div>
+        <div>
+          <button
+            onClick={this.handlePrevious}
+            disabled={currentNumber ? false : true}
+          >
+            Prev
+          </button>
+          <button
+            onClick={this.handleNext}
+            disabled={currentNumber === arr.length - 1 ? true : false}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/questions.js
@@ -0,0 +1,12 @@
+export const arr = [
+  {
+    question: "Select a color.",
+    options: ["black", "gold", "yellow", "white"],
+    answer: 3
+  },
+  {
+    question: "Select a colorwC.",
+    options: ["black", "gold", "yellow", "white"],
+    answer: 3
+  }
+];
