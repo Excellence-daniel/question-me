784885e7bde3f5b45f2f0e58d7ef955d46118add
Daniel <eoyeniran@tradedept.co>
2020-02-17 11:51:13 +0100

    added questions, calculated total, rendered failed questions and their correct answers

--- src/App.js
+++ src/App.js
@@ -9,32 +9,98 @@ class App extends Component {
     questionsTotal: 0,
     currentNumber: 0,
     correctAnswerIndex: null,
-    totalScores: 0
+    totalScores: 0,
+    questionTimer: 0,
+    startedTest: false,
+    disabledNext: true,
+    finishTest: false,
+    failedQuestions: []
   };
+
   componentDidMount = () => {
-    this.setState({ questionsTotal: arr.length });
+    this.setState({
+      questionsTotal: arr.length,
+      questionTimer: arr[0].time || 0
+    });
+  };
+
+  startTest = () => {
+    this.setState({ startedTest: true });
+  };
+
+  finishTest = () => {
+    this.setState({ finishTest: true });
   };
 
   selectAnswer = e => {
-    const { currentNumber, totalScores } = this.state;
+    this.setState({ disabledNext: false });
+    const { currentNumber, totalScores, failedQuestions } = this.state;
     const picked = Number(e.target.value);
     const correctAnswerIndex = arr[currentNumber].answer;
     const score = arr[currentNumber].score;
+    console.log({
+      picked,
+      correctAnswerIndex,
+      score,
+      currentNumber
+    });
     if (picked === correctAnswerIndex) {
       this.setState({ totalScores: totalScores + score });
+      console.log({ totalScores: totalScores + score });
+    } else {
+      failedQuestions.push({
+        question: arr[currentNumber].question,
+        correctAnswer: arr[currentNumber].options[correctAnswerIndex],
+        pickedAnswer: arr[currentNumber].options[picked]
+      });
     }
     this.setState({
       disableButton: true,
       pickedIndex: picked,
       correctAnswerIndex
     });
+    // this.countTime();
   };
+
+  countTime = questionTime => {
+    if (questionTime > 0) {
+      // setInterval(() => {
+      //   const newTime = questionTime - 1;
+      //   console.log(newTime);
+      //   this.countTime();
+      //   return;
+      // }, 1000);
+    }
+    return;
+  };
+
+  // countTime = questionTime => {
+  //   console.log({ questionTime });
+
+  //   if (questionTime > 0) {
+  //     const interval = setInterval(() => {
+  //       this.setState({ questionTimer: questionTime - 1 },  () => {
+  //         // this.countTime(questionTime - 1);
+  //         const newTime = questionTime - 1;
+  //         // this.countTime(newTime) ;
+  //         return;
+  //       });
+  //     }, 1000);
+  //     return () => clearInterval(interval);
+  //   } else {
+  //     console.log("here");
+  //     return;
+  //   }
+  // };
 
   handlePrevious = () => {
     const { currentNumber } = this.state;
+    let previousNumber = currentNumber - 1;
+    const questionTime = arr[previousNumber].time;
     if (currentNumber > 0) {
       this.setState({
-        currentNumber: currentNumber - 1
+        currentNumber: previousNumber,
+        questionTimer: questionTime
       });
       this.resetStateAnswers();
     }
@@ -42,11 +108,32 @@ class App extends Component {
 
   handleNext = () => {
     const { currentNumber } = this.state;
+    let nextNumber = currentNumber + 1;
+    const questionTime = arr[nextNumber].time;
     if (currentNumber < arr.length) {
       this.setState({
-        currentNumber: currentNumber + 1
+        currentNumber: nextNumber,
+        questionTimer: questionTime
       });
       this.resetStateAnswers();
+      this.countTime(questionTime);
+    }
+  };
+
+  sortGrade = grade => {
+    switch (true) {
+      case grade <= 30:
+        return "F";
+      case grade > 30:
+        return "D";
+      case grade > 60:
+        return "C";
+      case grade > 90:
+        return "B";
+      case grade > 120:
+        return "A";
+      default:
+        return "Score Thief!";
     }
   };
 
@@ -54,7 +141,8 @@ class App extends Component {
     this.setState({
       pickedIndex: null,
       correctAnswerIndex: null,
-      disableButton: false
+      disableButton: false,
+      disabledNext: true
     });
   };
 
@@ -64,62 +152,146 @@ class App extends Component {
       pickedIndex,
       currentNumber,
       correctAnswerIndex,
-      totalScores
+      totalScores,
+      questionTimer,
+      startedTest,
+      disabledNext,
+      finishTest,
+      failedQuestions
     } = this.state;
     return (
       <div className="questionnaire">
-        <div>
-          <p className="question">{arr[currentNumber].question}</p>
-          <div>
-            {arr[currentNumber].options.map((option, index) => {
-              return (
-                <button
-                  key={index}
-                  id={
-                    pickedIndex &&
-                    correctAnswerIndex !== null &&
-                    index === pickedIndex &&
-                    index === correctAnswerIndex &&
-                    pickedIndex === correctAnswerIndex
-                      ? "button-id-correct"
-                      : index === pickedIndex
-                      ? "button-id-selected"
-                      : index === correctAnswerIndex
-                      ? "button-id-correct"
-                      : "button-id"
-                  }
-                  className={
-                    disableButton ? "option-button disabled" : "option-button"
-                  }
-                  onClick={this.selectAnswer}
-                  value={index}
-                  disabled={disableButton}
-                >
-                  {option}
-                </button>
-              );
-            })}
+        {!finishTest ? (
+          !startedTest ? (
+            <div className="questions-info">
+              <h3>Test Quiz</h3>
+              <h4>
+                This set of questions are CSS, HTML and Javascript basic
+                questions. There are 15 in this set, with a total score of 170.
+              </h4>
+              <h4>Grades::</h4>
+              <p>
+                <ul>
+                  <li>
+                    <b>A</b> : > 120
+                  </li>
+                  <li>
+                    <b>B</b> : > 90
+                  </li>
+                  <li>
+                    <b>C</b> : > 60
+                  </li>
+                  <li>
+                    <b>D</b> : > 30
+                  </li>
+                  <li>
+                    <b>F</b> : less/= 30
+                  </li>
+                </ul>
+              </p>
+              <h4>
+                There is a timer on each question and an immediate display of
+                scores as soon as you answer your question.
+              </h4>
+
+              <h2>Wish you best of luck! Daniel :)</h2>
+              <button className="start-test" onClick={this.startTest}>
+                Start Test
+              </button>
+            </div>
+          ) : (
+            <>
+              <div className="question-div">
+                <div>
+                  <p className="question">{arr[currentNumber].question}</p>
+                  <div>
+                    {arr[currentNumber].options.map((option, index) => {
+                      return (
+                        <button
+                          key={index}
+                          id={
+                            pickedIndex &&
+                            correctAnswerIndex !== null &&
+                            index === pickedIndex &&
+                            index === correctAnswerIndex &&
+                            pickedIndex === correctAnswerIndex
+                              ? "button-id-correct"
+                              : index === pickedIndex
+                              ? "button-id-selected"
+                              : index === correctAnswerIndex
+                              ? "button-id-correct"
+                              : "button-id"
+                          }
+                          className={
+                            disableButton
+                              ? "option-button disabled"
+                              : "option-button"
+                          }
+                          onClick={this.selectAnswer}
+                          value={index}
+                          disabled={disableButton}
+                        >
+                          {option}
+                        </button>
+                      );
+                    })}
+                  </div>
+                </div>
+                <div>
+                  <button
+                    onClick={this.handlePrevious}
+                    // disabled={currentNumber ? false : true}
+                    disabled={true}
+                  >
+                    Prev
+                  </button>
+                  {currentNumber === arr.length - 1 ? (
+                    <button onClick={this.finishTest}>Finish</button>
+                  ) : (
+                    <button onClick={this.handleNext} disabled={disabledNext}>
+                      Next
+                    </button>
+                  )}
+                </div>
+                <div className="total-scores">
+                  <p>
+                    Total Scores : <b>{totalScores}</b>
+                  </p>
+                </div>
+              </div>
+              <div className="timer-div">{questionTimer}</div>
+            </>
+          )
+        ) : (
+          <div className="finish-test">
+            <h3>Results</h3>
+            <h4>
+              {failedQuestions.map((qU, index) => {
+                return (
+                  <>
+                    <p className="test-question">
+                      <b>
+                        {index + 1}. {qU.question}
+                      </b>
+                    </p>
+                    <p className="picked-answer">
+                      <span className="picked"> Picked: </span>
+                      <span className="value">{qU.pickedAnswer}</span>
+                    </p>
+                    <p className="correct-answer">
+                      <span className="correct">Correct Answer: </span>
+                      <span className="value">{qU.correctAnswer}</span>
+                    </p>
+                  </>
+                );
+              })}
+            </h4>
+            <h4>
+              Grade:{" "}
+              <span className="grade">{this.sortGrade(totalScores)}</span>
+            </h4>
           </div>
-        </div>
-        <div>
-          <button
-            onClick={this.handlePrevious}
-            disabled={currentNumber ? false : true}
-          >
-            Prev
-          </button>
-          <button
-            onClick={this.handleNext}
-            disabled={currentNumber === arr.length - 1 ? true : false}
-          >
-            Next
-          </button>
-        </div>
-        <div className="total-scores">
-          <p>
-            Total Scores : <b>{totalScores}</b>
-          </p>
-        </div>
+        )}
       </div>
     );
   }
--- src/questions.js
+++ src/questions.js
@@ -1,14 +1,138 @@
 export const arr = [
   {
-    question: "Select a color.",
-    options: ["black", "gold", "yellow", "white"],
-    answer: 3,
-    score: 20
+    question: "HTML stands for?",
+    options: [
+      "HyperText Markup Language",
+      "Hyper Tax Makes Line",
+      "Holistic Technical Method Library",
+      "HyperText Text MarkUp Language"
+    ],
+    answer: 0,
+    score: 20,
+    time: 8
   },
   {
-    question: "Select a colorwC.",
-    options: ["black", "gold", "yellow", "white"],
+    question: "One of these is not a Javascript Data Type.",
+    options: ["Boolean", "Undefined", "Int", "Object"],
+    answer: 2,
+    score: 15,
+    time: 10
+  },
+  {
+    question: "HTML tags are enclosed in ",
+    options: ["# and #", "* and *", "{ and }", "< and >"],
     answer: 3,
-    score: 5
+    score: 10,
+    time: 9
+  },
+  {
+    question:
+      "How do you make each word in a text start with a capital letter?",
+    options: [
+      "text-transform : capitalize",
+      "text-transform : uppercase",
+      "text-transform : big-case",
+      "text-style: uppercase"
+    ],
+    answer: 1,
+    score: 15,
+    time: 10
+  },
+  {
+    question: "Which of the following property sets the font size of text?",
+    options: ["text-size", "size", "font-size", "text-style"],
+    answer: 2,
+    score: 7,
+    time: 10
+  },
+  {
+    question:
+      "Where in an HTML document is the correct place to refer to an external style sheet?",
+    options: [
+      "In the <body> section",
+      "In the <head> section",
+      "After the <link> section",
+      "After the <head> section"
+    ],
+    answer: 1,
+    score: 5,
+    time: 6
+  },
+  {
+    question: "One of these is NOT a way to define a variable in Javascript.",
+    options: ["Let", "Int", "Const", "Var"],
+    answer: 1,
+    score: 9,
+    time: 12
+  },
+  {
+    question: "One of these is NOT a way to empty an array.",
+    options: [
+      "array = []",
+      "array.splice(0, array.length)",
+      "array.empty()",
+      "array.length = 0"
+    ],
+    answer: 2,
+    score: 12,
+    time: 11
+  },
+  {
+    question: "Which is the correct CSS syntax?",
+    options: [
+      "{body; color = 'black'}",
+      "{body : color:'black'}",
+      "{body : color = 'black'}",
+      "body{color:'black'}"
+    ],
+    answer: 1,
+    score: 18,
+    time: 10
+  },
+  {
+    question: "What is undefined x NaN?",
+    options: ["undefined", "0", "NaN", "1"],
+    answer: 2,
+    score: 15,
+    time: 10
+  },
+  {
+    question: "The first tag inside a <TABLE> is",
+    options: ["<HEAD>", "<CAPTION>", "<THEAD>", "<LINK>"],
+    answer: 2,
+    score: 10,
+    time: 16
+  },
+  {
+    question: "Comments in Javascript is written with: ",
+    options: ["//", "/ /", "/* */", "* *"],
+    answer: 0,
+    score: 5,
+    time: 5
+  },
+  {
+    question: "Which tag can set the background color of your page?",
+    options: ["<title>", "<link>", "<body>", "<head>"],
+    answer: 2,
+    score: 3,
+    time: 12
+  },
+  {
+    question:
+      "Which of these is used when you want nothing to override a css style?",
+    options: [".no-override", "input['radio']", "!important", "position:fixed"],
+    answer: 2,
+    score: 20,
+    time: 8
+  },
+  {
+    question: `var employee = {'company':'xyz'};
+      var empl = Object.create(employee); 
+      delete empl.company;. 
+      What would this code output?`,
+    options: ["{}", "undefined", "xyz", "{empl}"],
+    answer: 2,
+    score: 6,
+    time: 12
   }
 ];
